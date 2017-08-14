dd99ede9ea416e9aeb517a45d5ea53024962db09
Ross Hinkley <rosshinkley@gmail.com>
2017-08-14 15:40:24 -0400

    got hap working reliably, still should clean up the conf, but... later

--- haproxy/hap.sh
+++ haproxy/hap.sh
@@ -3,9 +3,12 @@
 docker build -t ddp-haproxy .
 
 if docker container ls -al | grep -q my-sweet-proxy; then
+  # stop the container
+  docker container stop my-sweet-proxy
+  # torch the container
   docker container rm my-sweet-proxy
 fi
 
-docker run -itd --network ddp --name my-sweet-proxy ddp-haproxy
+docker run -itd --network ddp -p 80:2222 --name my-sweet-proxy ddp-haproxy
 
 
